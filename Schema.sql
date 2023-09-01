61d4b9bfa8baeda2e377978fc60c22d1a13dfc8a
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-09-01 13:31:28 +0300

    query multiple tables

--- Schema.sql
+++ Schema.sql
@@ -7,3 +7,18 @@ CREATE TABLE animals (
     weight_kg DECIMAL(5, 2)
 )
 ALTER TABLE animals ADD COLUMN species VARCHAR(150);
+
+CREATE TABLE owners (
+	id SERIAL PRIMARY KEY,
+	full_name VARCHAR(100),
+	age INT
+);	
+CREATE TABLE species (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100)
+);
+
+ALTER TABLE animals
+    DROP COLUMN species,
+    ADD COLUMN species_id INT,
+    ADD COLUMN owner_id INT;
--- data.sql
+++ data.sql
@@ -13,3 +13,26 @@ VALUES
     ('Boarmon', '2005-06-07', 7, TRUE, 20.4),
     ('Blossom', '1998-10-13', 3, TRUE, 17),
     ('Ditto', '2022-05-14', 4, TRUE, 22);
+
+INSERT INTO owners (full_name, age)
+VALUES
+    ('Sam Smith', 34),
+	('Jennifer Orwell', 34),
+	('Bob', 34),
+	('Melody Pond', 34),
+	('Dean Winchester', 34),
+	('Jodie Whittaker', 34);
+
+INSERT INTO species (name)
+VALUES
+      ('Pokemon'),
+      ('Digimon');
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+
+UPDATE animals SET owner_id = 1 WHERE name='Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
